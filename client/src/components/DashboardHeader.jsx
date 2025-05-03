4e9c4d7e393648e6592ec14c9f114aee33523d37
LEELAMANOHARGUDIVADA <leelamanohar.gudivada@gmail.com>
2025-05-03 16:03:22 +0530

    Add Avatar

--- client/src/components/DashboardHeader.jsx
+++ client/src/components/DashboardHeader.jsx
@@ -1,10 +1,17 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-export default function DashboardHeader(){
+export default function DashboardHeader() {
     const username = localStorage.getItem("username");
     return (
         <div className="w-full py-1">
-            <div className="w-full h-full flex items-center justify-between">
+            <div className="w-[75%] h-full flex items-center justify-between px-5">
                 <h4 className="font-bold">Welcome 👋 , {username}</h4>
+                <div className="flex items-center justify-center gap-5">
+                    <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                </div>
             </div>
         </div>
     )
