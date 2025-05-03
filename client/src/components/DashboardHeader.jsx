85a36a1430919891d364dc5e9e1cb3e8815f1ad6
LEELAMANOHARGUDIVADA <leelamanohar.gudivada@gmail.com>
2025-05-03 16:11:12 +0530

    Add Logout button

--- client/src/components/DashboardHeader.jsx
+++ client/src/components/DashboardHeader.jsx
@@ -1,16 +1,41 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LogOut } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function DashboardHeader() {
+    const navigate = useNavigate();
+    const handleLogOut = () => {
+        localStorage.clear();
+        navigate('/login');
+    }
     const username = localStorage.getItem("username");
     return (
         <div className="w-full py-1">
             <div className="w-[75%] h-full flex items-center justify-between px-5">
                 <h4 className="font-bold">Welcome 👋 , {username}</h4>
                 <div className="flex items-center justify-center gap-5">
-                    <Avatar>
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
+                    <DropdownMenu >
+                        <DropdownMenuTrigger className="outline-none cursor-pointer">
+                        <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem onClick={() => handleLogOut()} >
+                                <div className="flex items-center justify-center gap-2">
+                                    <LogOut size={15} className='text-red-500' />
+                                    <span className='text-red-500 text-xs'>Log Out</span>
+                                </div>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </div>
             </div>
         </div>
